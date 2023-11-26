6e37a8da4bb766a999701c1273024d969e19b29d
ohyw <vvjiango@gmail.com>
2023-11-26 10:41:12 +0800

    选修课

--- huawei/src/main/java/一百分/选修课.java
+++ huawei/src/main/java/一百分/选修课.java
@@ -14,82 +14,53 @@ public class 选修课 {
         String str1 = input.nextLine();
         String str2 = input.nextLine();
 
-        new ElectiveCourse().solve(str1, str2);
+        // 报名了课程2的学生转换成hashmap
+        StringTokenizer st1 = new StringTokenizer(str1, ";");
+        StringTokenizer st2 = new StringTokenizer(str2, ";");
+        HashMap<String, Integer> st2map = new HashMap<>();
+        while (st2.hasMoreElements()) {
+            String[] idandscore = st2.nextToken().split(",");
+            st2map.put(idandscore[0], Integer.parseInt(idandscore[1]));
+        }
+        // 查找同时报名了2门课程的学生
+        Set<String> fiveids = new TreeSet<>();
+        Map<String, Set<Student>> students = new HashMap<>();
+        while (st1.hasMoreElements()) {
+            String[] idandscore = st1.nextToken().split(",");
+            String id = idandscore[0];
+            String fiveid = id.substring(0, 5);
+            int score = Integer.parseInt(idandscore[1]);
+            if (st2map.containsKey(id)) {
+                fiveids.add(fiveid);
+                students.computeIfAbsent(fiveid, key -> new TreeSet<>()).add(new Student(id, score));
+            }
+        }
+
+        if (fiveids.isEmpty()) {
+            System.out.println("NULL");
+            return;
+        }
+        for (String fiveid : fiveids) {
+            System.out.println(fiveid);
+            StringBuilder sb = new StringBuilder();
+            for (Student stu : students.get(fiveid)) {
+                sb.append(stu.id).append(";");
+            }
+            System.out.println(sb.substring(0, sb.length() - 1));
+        }
     }
 }
 
-class ElectiveCourse {
-    class Student implements Comparable<Student> {
-        String id;
-        int score;
-        public Student(String id, int score) {
-            this.id = id;
-            this.score = score;
-        }
-        @Override
-        public int compareTo(Student o) {
-            return score != o.score ? Integer.compare(score, o.score) : id.compareTo(o.id);
-        }
-    }
-    public void solve(String str1, String str2) {
-        //把数据转换成hash表，key-value：学号-成绩
-        HashMap<String, Integer> student_one = convert(str1);
-        HashMap<String, Integer> student_two = convert(str2);
-
-        //找出同时报了两门选修课的学生信息，合并成绩，剔除掉其他学生信息，结果保留在student_one中
-        search(student_one, student_two);
-
-        //按照要求输出结果
-        if (student_one.isEmpty()) {
-            System.out.println("NULL");
-        }
-        /*
-        1.前5位学号升序排列
-        2.同学号按两门成绩之和升序排列
-        3.同成绩按后3位升序排列
-        */
-        //转成格式：hashmap<前5位, Set<完整学号, 总分数>>
-        Set<String> fiveids = new TreeSet<>();
-        Map<String, Set<Student>> students = new HashMap<>();
-
-        Iterator<Map.Entry<String, Integer>> iterator = student_one.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, Integer> entry = iterator.next();
-            String fiveid = entry.getKey().substring(0, 5);
-            fiveids.add(fiveid);
-            students.computeIfAbsent(fiveid, k -> new TreeSet<>()).add(new Student(entry.getKey(), entry.getValue()));
-        }
-
-        for (String fiveid : fiveids) {
-            System.out.println(fiveid);
-            StringBuilder s = new StringBuilder();
-            for (Student stu : students.get(fiveid)) {
-                s.append(stu.id).append(";");
-            }
-            System.out.println(s.substring(0, s.length() - 1));
-        }
+class Student implements Comparable<Student> {
+    String id;
+    int score;
+    public Student(String id, int score) {
+        this.id = id;
+        this.score = score;
     }
 
-    private void search(HashMap<String, Integer> source, HashMap<String, Integer> target) {
-        Iterator<Map.Entry<String, Integer>> iterator = source.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, Integer> entry = iterator.next();
-            if (target.containsKey(entry.getKey())) {
-                entry.setValue(entry.getValue() + target.get(entry.getKey()));
-            } else {
-                iterator.remove();
-            }
-        }
-    }
-
-    private HashMap<String, Integer> convert(String string) {
-        HashMap<String, Integer> map = new HashMap<>();
-        //String格式：xxxxxxxx,xx;xxxxxxxx,xx
-        StringTokenizer st = new StringTokenizer(string, ";");
-        while (st.hasMoreElements()) {
-            String[] ss = st.nextToken().split(",");
-            map.put(ss[0], Integer.valueOf(ss[1]));
-        }
-        return map;
+    @Override
+    public int compareTo(Student o) {
+        return score != o.score ? Integer.compare(score, o.score) : id.compareTo(o.id);
     }
 }
